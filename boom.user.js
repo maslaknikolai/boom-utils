0e4b966dd2523ea2d34a09b674341c24be303004
Nikolai Maslak <nikolai.maslak@poleinvest.ru>
2021-11-05 00:31:24 +0300

    userscript started

---
+++ boom.user.js
@@ -0,0 +1,77 @@
+// ==UserScript==
+// @name           Boom utils
+// @version        1.0.0
+// @description    Делать вещи быстрее
+// @author         Maslak Nikolai
+// @include        https://maslaknikolai.github.io/boom-utils/test-us.html*
+// @grant          GM.xmlHttpRequest
+// @grant          GM_xmlhttpRequest
+// @grant          GM_download
+// @grant          GM_info
+// @grant          GM_setValue
+// @grant          GM_getValue
+// @grant          GM_deleteValue
+// @grant          GM_listValues
+// @grant          GM_addValueChangeListener
+// @grant          GM_notification
+// @grant          GM.setValue
+// @grant          GM.getValue
+// @grant          GM.deleteValue
+// @grant          GM.listValues
+// @grant          unsafeWindow
+// @grant          GM.addValueChangeListener
+// @run-at         document-end
+// @allFrames      true
+// ==/UserScript==
+
+const wait = (ms) => new Promise(r => setTimeout(r, ms))
+async function start() {
+    const boomUtilsDataRaw = getQuery().boomUtilsData
+    if (!boomUtilsDataRaw) {
+        return
+    }
+
+    const boomUtilsData = JSON.parse(boomUtilsDataRaw)
+
+    if (boomUtilsData.action === 'fillAlbum') await fillAlbum(boomUtilsData)
+}
+
+start()
+
+async function fillAlbum(boomUtilsData) {
+    const ids = boomUtilsData.ids
+
+    const addInputEl = document.querySelector('.add-count')
+    const addBtnEl = document.querySelector('.add-btn')
+
+    addInputEl.value = ids.length
+    addBtnEl.click()
+
+    await wait(500)
+
+    Array.from(document.querySelectorAll('.track-id'))
+        .slice(0, ids.length)
+        .forEach((trackIdInputEl, i) => {
+            trackIdInputEl.value = ids[i]
+        })
+}
+
+//
+//
+// utils
+//
+//
+
+
+function getQuery() {
+    return Object.fromEntries(
+        location.search
+            .slice(1)
+            .split('&')
+            .map((searchParam) => {
+                const sp = searchParam.split('=')
+                return [sp[0], decodeURIComponent(sp[1])]
+            })
+            .filter((i) => i[0])
+    )
+}
