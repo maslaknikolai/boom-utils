09211c0cfc863df46cedb4b1b60b6f83ea2ed4e8
Nikolay Maslak <nikolay.maslak@onlinesup.com>
2021-11-07 20:02:17 +0300

    user script v1

--- boom.user.js
+++ boom.user.js
@@ -3,7 +3,7 @@
 // @version        1.0.1
 // @description    Делать вещи быстрее
 // @author         Maslak Nikolai
-// @include        https://maslaknikolai.github.io/boom-utils/test-us.html*
+// @include        https://control.um-agency.com/*
 // @grant          GM.xmlHttpRequest
 // @grant          GM_xmlhttpRequest
 // @grant          GM_download
@@ -20,6 +20,7 @@
 // @grant          GM.listValues
 // @grant          unsafeWindow
 // @grant          GM.addValueChangeListener
+// @require        https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js
 // @run-at         document-end
 // @allFrames      true
 // ==/UserScript==
@@ -32,25 +33,65 @@ async function start() {
 
     const boomUtilsData = JSON.parse(boomUtilsDataRaw)
 
-    if (boomUtilsData.action === 'fillAlbum') await fillAlbum(boomUtilsData)
+    if (boomUtilsData.action === 'editPlaylist') await editPlaylist(boomUtilsData)
+    if (boomUtilsData.action === 'fillPlaylist') await fillPlaylist(boomUtilsData)
 }
 
 start()
 
-async function fillAlbum(boomUtilsData) {
+async function editPlaylist(boomUtilsData) {
     const ids = boomUtilsData.ids
 
-    const addInputEl = document.querySelector('.add-count')
-    const addBtnEl = document.querySelector('.add-btn')
+    const btns = Array.from(document.querySelectorAll('.sonata-ba-list-field.sonata-ba-list-field-actions a'))
 
-    addInputEl.value = ids.length
+    if (btns.length === 0) {
+      return
+    }
+
+    const fillPlaylistData = {
+      action: 'fillPlaylist',
+      ids
+    }
+
+    btns.forEach((btn) => {
+      btn.href = `${btn.href}?boomUtilsData=${encodeURIComponent(JSON.stringify(fillPlaylistData))}`
+      btn.style.background = 'rgb(238 241 28)'
+    })
+
+    if (btns.length === 1) {
+      btns[0].click()
+    }
+}
+
+async function fillPlaylist(boomUtilsData) {
+    const ids = boomUtilsData.ids
+
+    const date = moment()
+    const dateStr = date
+      .startOf('day')
+      .day(4)
+      .format('YYYY-MM-DD HH:mm')
+
+    await sleep(1000)
+
+    const importDateInput = document.querySelector('[name*=import_date')
+    importDateInput.value = dateStr
+    importDateInput.style.background = 'rgb(238 241 28)'
+
+    const elementsCount = document.querySelector('#elementsCount')
+    const addBtnEl = elementsCount.parentElement.parentElement.querySelector('[title="Добавить новый"]')
+
+    elementsCount.value = ids.length
     addBtnEl.click()
 
-    await sleep(500)
+    await sleep(2000)
 
-    Array.from(document.querySelectorAll('.track-id'))
+    const idsInputs = document.querySelectorAll('.sonata-ba-tbody.ui-sortable td:nth-child(4) input')
+
+    Array.from(idsInputs)
         .slice(0, ids.length)
         .forEach((trackIdInputEl, i) => {
+            trackIdInputEl.style.background = 'rgb(238 241 28)'
             trackIdInputEl.value = ids[i]
         })
 }
@@ -60,8 +101,6 @@ async function fillAlbum(boomUtilsData) {
 // utils
 //
 //
-
-console.log(2);
 
 function sleep(ms){
     return new Promise(r => setTimeout(r, ms))
